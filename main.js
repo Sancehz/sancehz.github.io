559db72875de61cb8610661730a95f31a7ef07dd
Sancehz <135662842+Sancehz@users.noreply.github.com>
2024-09-13 11:58:04 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,45 @@
 var map = L.map(
 	'map', {
 	center: [9.86568, -83.891875],
-	zoom: 18
+	zoom: 19
 	}
 );
 
+function formatoMapa(feature) {
+	if(feature.geometry.type == "Point")
+		return formatoPuntos(feature);
+	if(feature.geometry.type == "LineString")
+		return formatoLineas(feature);
+	if(feature.geometry.type == "Polygon")
+		return formatoPolys(feature)
+}
+
+function formatoPuntos(f) {
+	var style = {color: "gray"};
+	return style;
+}
+
+function formatoLineas(f) {
+	var style = {color: "green"};
+	return style;
+}
+
+function formatoPolys(f) {
+	var style = {color: "blue"};
+	return style;
+}
+
 async function crearMapa() {
-	// Obtenemos el GeoJSON (lo hago asi para que el codigo no se vea tan feo)
-	// (((y para no tener que cambiar todo cuando actualizamos el mapa)))
+	// Obtenemos el GeoJSON como archivo de texto desde la misma pagina github
+	// (para no tener que cambiar todo cada vez que actualizamos algo)
 	var response = await fetch("mapa.json");
 	var mapaDat = await response.text();
 
-	var pJson = JSON.parse(mapaDat);
+	var pJson = JSON.parse(mapaDat); // cargamos el JSON como un objeto
 
+	// agregamos cada elemento
 	for(var feature of pJson.features) {
-		L.geoJson(feature).addTo(map);
-
+		L.geoJson(feature, {style: formatoMapa}).addTo(map);
 	}
 }
 
