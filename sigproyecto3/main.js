2b5e0e34ed30dd595756d0ec39774aa8f126c6ef
Sancehz <135662842+Sancehz@users.noreply.github.com>
2024-11-24 13:20:19 -0600

    Update main.js

--- sigproyecto3/main.js
+++ sigproyecto3/main.js
@@ -4,10 +4,23 @@
 var map = L.map(
 	'map', {
 		center: [10.6667, -85.4702],
-		zoom: 10
+		zoom: 10,
+		minZoom: 10,
+		maxZoom: 16,
 	}
 );
 
-L.tileLayer('http://sancehz.github.io/sigproyecto3/tiles/{z}/{x}/{y}.png', {
-    attribution: ''
-}).addTo(map);
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+var tilesLiberia = L.tileLayer('https://sancehz.github.io/sigproyecto3/tiles/{z}/{x}/{y}.png', {
+    attribution: '',
+	minZoom: 10,
+	maxZoom: 15,
+	errorTileUrl: "./imagenes/notile.png",
+	tms: true,
+});
+
+tilesLiberia.on("tileerror", function(e) {console.log(`tileerror: ${e.url}`)});
+tilesLiberia.addTo(map);
